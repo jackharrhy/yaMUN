b65d6c4beb7d3d0ff7df3cc7a3feb8b8832e00a7
Mudkip <me@mudkip.pw>
2021-02-28 00:00:09 -0330

    three new schedule methods

--- src/backend/api/controllers/schedules.ts
+++ src/backend/api/controllers/schedules.ts
@@ -1,7 +1,6 @@
 import Ajv, { JSONSchemaType } from "ajv";
 import debugFactory from "debug";
 import express from "express";
-import Course from "../../models/course";
 import Schedule, { ISchedule } from "../../models/schedule";
 import { ISemester } from "../../models/semester";
 import User from "../../models/user";
@@ -42,6 +41,16 @@ const ScheduleValidator: JSONSchemaType<ICreateSchemaInput> = {
 const validate = ajv.compile(ScheduleValidator);
 
 const schedulesController = {
+  async get(req: express.Request, res: express.Response) {
+    const scheduleId = req.params.scheduleId;
+    const schedule = await Schedule.findById(scheduleId);
+    if (schedule) {
+      res.json(schedule);
+    } else {
+      res.sendStatus(404);
+    }
+  },
+
   async create(req: express.Request, res: express.Response) {
     if (validate(req.body)) {
       const user = await User.findById(req.body.ownerId);
@@ -52,6 +61,16 @@ const schedulesController = {
       } else {
         res.sendStatus(400);
       }
+    } else {
+      res.sendStatus(400);
+    }
+  },
+
+  async delete(req: express.Request, res: express.Response) {
+    const scheduleId = req.params.scheduleId;
+    const deleted = await Schedule.findByIdAndDelete(scheduleId);
+    if (deleted) {
+      res.json(deleted);
     } else {
       res.sendStatus(400);
     }
@@ -68,6 +87,24 @@ const schedulesController = {
       } else {
         res.sendStatus(400);
       }
+    } else {
+      res.sendStatus(400);
+    }
+  },
+
+  async removeCourse(req: express.Request, res: express.Response) {
+    const scheduleId = req.params.scheduleId;
+    const crn = Number(req.params.crn);
+    const schedule = await Schedule.findById(scheduleId);
+    if (schedule) {
+      const removed = await schedule.removeCourse(crn);
+      if (removed) {
+        res.json(schedule);
+      } else {
+        res.sendStatus(400);
+      }
+    } else {
+      res.sendStatus(400);
     }
   },
 };
--- src/backend/api/index.ts
+++ src/backend/api/index.ts
@@ -9,8 +9,12 @@ export default async ({ port = 4000 } = {}) => {
 
   app.get("/courses/:crn", coursesController.courseByCrn);
   app.post("/users", usersController.create);
+
+  app.get("/schedules/:scheduleId", schedulesController.get);
   app.post("/schedules", schedulesController.create);
   app.put("/schedules/:scheduleId/:crn", schedulesController.addCourse);
+  app.delete("/schedules/:scheduleId/:crn", schedulesController.removeCourse);
+  app.delete("/schedules/:scheduleId", schedulesController.delete);
 
   return {
     listen: () => {
