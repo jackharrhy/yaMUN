e7e5d687819be74f2c565fb4db5eba7e1543d586
Mudkip <me@mudkip.pw>
2021-03-06 14:20:13 -0330

    small fixes

--- test/backend/models/bookmark.spec.ts
+++ test/backend/models/bookmark.spec.ts
@@ -1,13 +1,10 @@
 import { use, expect } from "chai";
 import { describe } from "mocha";
-import chaiAsPromised from "chai-as-promised";
 
 import Bookmark from "../../../src/backend/models/bookmark";
 import User from "../../../src/backend/models/user";
 import { testSemesterCrns } from "../../setup.spec";
 import { NotFoundError } from "../../../src/backend/api/errors";
-
-use(chaiAsPromised);
 
 describe("backend/models/bookmark", function () {
   let userId: string;
@@ -16,13 +13,13 @@ describe("backend/models/bookmark", function () {
     await Bookmark.deleteMany({});
     await User.deleteMany({});
 
-    await User.createUser("test", "test");
-    userId = (await User.findOne({}))._id;
+    userId = await User.createUser("test", "test")._id;
   });
 
   it("find/create user bookmark", async function () {
     const bookmark = await Bookmark.findOrCreateByUserId(userId);
     expect(bookmark.owner.toString()).to.equal(userId.toString());
+    expect(bookmark.courses).to.be.length(0);
   });
 
   it("create user bookmarks and add two courses to it", async function () {
@@ -37,11 +34,13 @@ describe("backend/models/bookmark", function () {
     const bookmark = await Bookmark.findOrCreateByUserId(userId);
     await bookmark.addCourse(testSemesterCrns[0]);
     expect(bookmark.courses).to.contain(testSemesterCrns[0]);
+    expect(bookmark.courses).to.be.length(1);
     await bookmark.removeCourse(testSemesterCrns[0]);
     expect(bookmark.courses).not.to.contain(testSemesterCrns[0]);
+    expect(bookmark.courses).to.be.length(0);
   });
 
-  it("create user bookmarks, add a non-existant course crn, should throw exception", async function () {
+  it("create user bookmarks, add a non-existant course crn", async function () {
     const bookmark = await Bookmark.findOrCreateByUserId(userId);
     await expect(bookmark.addCourse(123456)).to.be.rejectedWith(NotFoundError, "course not found");
   });
