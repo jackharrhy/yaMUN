9d1ef48ba826da653fddf938217b1231f0d5accd
Jack Arthur Harrhy <me@jackharrhy.com>
2021-03-08 09:38:34 -0330

    fix schedule model tests

--- test/backend/models/schedule.spec.ts
+++ test/backend/models/schedule.spec.ts
@@ -2,10 +2,10 @@ import { expect } from "chai";
 import { describe } from "mocha";
 import { Types } from "mongoose";
 
+import { NotFoundError } from "../../../src/backend/api/errors";
 import Schedule from "../../../src/backend/models/schedule";
 import User from "../../../src/backend/models/user";
-import { NotFoundError } from "../../../src/backend/api/errors";
-import { testSemester, testSemesterCrns } from "../../setup.spec";
+import { testSemester1, testSemester1Crns } from "../../test-data";
 
 describe("backend/models/schedule", function () {
   let userId: Types.ObjectId;
@@ -18,12 +18,18 @@ describe("backend/models/schedule", function () {
   });
 
   it("create schedule for a user", async function () {
-    const schedule = await Schedule.create({owner: userId, semester: testSemester});
+    const schedule = await Schedule.create({
+      owner: userId,
+      semester: testSemester1,
+    });
     expect(schedule).property("owner").to.equal(userId);
   });
 
   it("create schedule for a user, make it public, and change its title and description", async function () {
-    const schedule = await Schedule.create({owner: userId, semester: testSemester});
+    const schedule = await Schedule.create({
+      owner: userId,
+      semester: testSemester1,
+    });
     expect(schedule).property("owner").to.equal(userId);
     schedule.updateMeta("My Public Schedule", "A public schedule.", true);
     expect(schedule).property("title").to.equal("My Public Schedule");
@@ -32,38 +38,53 @@ describe("backend/models/schedule", function () {
   });
 
   it("create schedule for a user, add a course to it", async function () {
-    const schedule = await Schedule.create({owner: userId, semester: testSemester});
+    const schedule = await Schedule.create({
+      owner: userId,
+      semester: testSemester1,
+    });
     expect(schedule).property("owner").to.equal(userId);
     expect(schedule).property("courses").to.be.length(0);
-    await schedule.addCourse(testSemesterCrns[0]);
+    await schedule.addCourse(testSemester1Crns[0]);
     expect(schedule).property("courses").to.be.length(1);
   });
 
   it("create schedule for a user, add two courses to it and then delete one", async function () {
-    const schedule = await Schedule.create({owner: userId, semester: testSemester});
+    const schedule = await Schedule.create({
+      owner: userId,
+      semester: testSemester1,
+    });
     expect(schedule).property("owner").to.equal(userId);
     expect(schedule).property("courses").to.be.length(0);
-    await schedule.addCourse(testSemesterCrns[0]);
-    await schedule.addCourse(testSemesterCrns[1]);
+    await schedule.addCourse(testSemester1Crns[0]);
+    await schedule.addCourse(testSemester1Crns[1]);
     expect(schedule).property("courses").to.be.length(2);
-    await schedule.removeCourse(testSemesterCrns[1]);
+    await schedule.removeCourse(testSemester1Crns[1]);
     expect(schedule).property("courses").to.be.length(1);
   });
 
   it("create schedule for a user, and try to add course from another semester", async function () {
-    const schedule = await Schedule.create({owner: userId, semester: testSemester});
+    const schedule = await Schedule.create({
+      owner: userId,
+      semester: testSemester1,
+    });
     expect(schedule).property("owner").to.equal(userId);
     expect(schedule).property("courses").to.be.length(0);
-    await schedule.addCourse(testSemesterCrns[0]);
-    await schedule.addCourse(testSemesterCrns[1]);
+    await schedule.addCourse(testSemester1Crns[0]);
+    await schedule.addCourse(testSemester1Crns[1]);
     expect(schedule).property("courses").to.be.length(2);
-    await schedule.removeCourse(testSemesterCrns[1]);
+    await schedule.removeCourse(testSemester1Crns[1]);
     expect(schedule).property("courses").to.be.length(1);
   });
 
   it("create two different schedules for a user", async function () {
-    const scheduleOne = await Schedule.create({owner: userId, semester: testSemester});
-    const scheduleTwo = await Schedule.create({owner: userId, semester: testSemester});
+    const scheduleOne = await Schedule.create({
+      owner: userId,
+      semester: testSemester1,
+    });
+    const scheduleTwo = await Schedule.create({
+      owner: userId,
+      semester: testSemester1,
+    });
 
     expect(scheduleOne).property("owner").to.equal(userId);
     expect(scheduleTwo).property("owner").to.equal(userId);
@@ -72,8 +93,11 @@ describe("backend/models/schedule", function () {
     expect(scheduleOne._id).to.not.equal(scheduleTwo._id);
   });
 
-  it("create schedule, change meta, and then change it again", async function() {
-    const schedule = await Schedule.create({owner: userId, semester: testSemester});
+  it("create schedule, change meta, and then change it again", async function () {
+    const schedule = await Schedule.create({
+      owner: userId,
+      semester: testSemester1,
+    });
     expect(schedule).property("owner").to.equal(userId);
     schedule.updateMeta("My Public Schedule", "A public schedule.", true);
     expect(schedule).property("title").to.equal("My Public Schedule");
@@ -86,10 +110,16 @@ describe("backend/models/schedule", function () {
   });
 
   it("create schedule for a user, add invalid courses to it", async function () {
-    const schedule = await Schedule.create({owner: userId, semester: testSemester});
+    const schedule = await Schedule.create({
+      owner: userId,
+      semester: testSemester1,
+    });
     expect(schedule).property("owner").to.equal(userId);
     expect(schedule).property("courses").to.be.length(0);
-    const invalidCourseAdd =  schedule.addCourse(-1);
-    await expect(invalidCourseAdd).to.be.eventually.rejectedWith(NotFoundError, "course not found");
+    const invalidCourseAdd = schedule.addCourse(-1);
+    await expect(invalidCourseAdd).to.be.eventually.rejectedWith(
+      NotFoundError,
+      "course not found"
+    );
   });
 });
